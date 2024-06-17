fca0269fd9530cb722c569c13ee87bb95e4121e8
IderlanOliveira <118554164+IderlanOliveira@users.noreply.github.com>
2024-06-17 15:25:48 -0300

    Atualização 1.1

---
+++ tela-professor.js
@@ -0,0 +1,7 @@
+document.getElementById('showContentButton').addEventListener('click', function() {
+    document.getElementById('newContent').classList.toggle('hidden');
+});
+
+function additionalFunction() {
+    document.getElementById('additionalContent').classList.toggle('hidden');
+}
